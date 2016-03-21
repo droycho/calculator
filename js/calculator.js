24054069d78fbb35effa386d52194798472c1381
DJ Roychoudhury and Sam Durfey <sam@>
2016-03-21 09:33:47 -0700

    created addition calculator app

---
+++ js/calculator.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2) {
+  return parseInt(number1) + parseInt(number2);
+};
+alert(add(10,5));
+var number1 = prompt("Enter a number:");
+var number2 = prompt("Enter another number:");
+var result = add(number1, number2);
+alert(result);
