ed623faa30af76b58ff6c8ac95e144571ab9f7b3
DJ Roychoudhury and Sam Durfey <dj@>
2016-03-21 13:26:50 -0700

    added README.md

--- js/cookingconversions.js
+++ js/cookingconversions.js
@@ -1,25 +1,25 @@
 var poundsToKilograms = function() {
-	var input = parseInt(prompt("what is the weight in Pounds"));
+	var input = parseInt(prompt("What is the weight in Pounds?"));
 	var output = (input * 0.453592);
   alert("The weight is " + output + "kgs.");
 };
 
 var ounceToGram = function() {
-	var input = parseInt(prompt("what is the weight in ounces"));
+	var input = parseInt(prompt("What is the weight in ounces?"));
 	var output = (input * 28.3495);
   alert("The weight is " + output + "gs.");
 };
 
 var pintToLiter = function() {
-	var input = parseInt(prompt("what is the volume in pints?"));
+	var input = parseInt(prompt("What is the volume in pints?"));
 	var output = (input * 0.473176);
-  alert("The weight is " + output + "l.");
+  alert("The volume is " + output + "l.");
 };
 
 var flozToMl = function() {
-	var input = parseInt(prompt("what is the volume in Fluid Ounces?"));
+	var input = parseInt(prompt("What is the volume in Fluid Ounces?"));
 	var output = (input * 29.5735);
-  alert("The weight is " + output + "ml.");
+  alert("The volume is " + output + "ml.");
 };
 
 poundsToKilograms();
